4ffe1d9b9ad2a3025fef4af11cadec0c543773ff
chocolaggibbiddori <didrkd684@naver.com>
2023-03-27 11:15:40 +0900

    영속성 컨텍스트 2
    2) 첫번째 조회는 SELECT 쿼리가 나가지만 두번째 조회에서는 나가지 않음(첫번째 조회 시 DB에서 1차 캐시로 저장하고 두번째 조회시에는 1차 캐시에서 데이터를 조회하기 때문)
    3) L21 : 동일성을 보장해줌

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,18 +15,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
-            System.out.println("=== BEFORE ===");
-            em.persist(member); //영속
-            System.out.println("=== AFTER ===");
-
-            Member findMember = em.find(Member.class, 101L);
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
+            System.out.println("result = " + (findMember1 == findMember2));
 
             tx.commit();
         } catch (Exception e) {
