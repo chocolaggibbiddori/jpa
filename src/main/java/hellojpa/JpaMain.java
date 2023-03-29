1ee077866f91e0fee55c922255d100e5913651dc
chocolaggibbiddori <didrkd684@naver.com>
2023-03-29 16:39:14 +0900

    양방향 연관관계 주의
    양쪽을 둘 다 셋팅해주어야 함
    *그렇지 않으면 em.flush(); em.clear();를 호출하지 않았을 때 값이 읽히지 않음

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -16,24 +15,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
+            member.changeTeam(team);
             em.persist(member);
 
             em.flush();
             em.clear();
 
-            //조회
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
-
-            for (Member m : members) {
+            Team findTeam = em.find(Team.class, team.getId());
+            for (Member m : findTeam.getMembers()) {
                 System.out.println("m = " + m.getUsername());
             }
 
